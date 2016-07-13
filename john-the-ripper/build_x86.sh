d88ba543636db3261015823e0f4258430f3daf79
Claudio André <claudioandre.br@gmail.com>
2016-07-13 15:00:11 -0300

    Avoid extra builds.

--- john-the-ripper/build_x86.sh
+++ john-the-ripper/build_x86.sh
@@ -4,20 +4,19 @@
 #snapcraft clean --step build libs
 #rm parts/john-the-ripper/build/run/john-*
 
-#cd parts/john-the-ripper/build/src
-#git fetch --unshallow
+MYPWD=$(pwd)
 
-cd parts/john-the-ripper
-git clone --depth 1 https://github.com/magnumripper/JohnTheRipper.git build
-cd build/src
-git fetch --unshallow
+git clone https://github.com/magnumripper/JohnTheRipper.git tmp
+mv tmp/* ./
+cd src
 
 wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/0001-Handle-self-confined-system-wide-build.patch
 wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/Temporary%20-%20disable%20nice
 patch < 0001-Handle-self-confined-system-wide-build.patch
 patch < Temporary\ -\ disable\ nice
 
-make distclean
+./configure --disable-native-tests --with-systemwide --disable-openmp CPPFLAGS='-D_SNAP' && make -s clean && make -sj2 && mv ../run/john ../run/john-opencl-non-omp       &&
+./configure --disable-native-tests --with-systemwide CPPFLAGS='-D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-opencl-non-omp\""' && make -s clean && make -sj2 && mv ../run/john ../run/john-opencl   
 
 ./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS='-D_SNAP' && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2-non-omp       &&
 ./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-sse2-non-omp\""' && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2       &&
@@ -28,6 +27,4 @@ make distclean
 ./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx2 -D_SNAP' --disable-openmp && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp       &&
 ./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx2 -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-non-omp\"" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-xop\""' && make -s clean && make -sj2 
 
-cd -
-rm -rf parts/john-the-ripper/build/run/kernels
-
+cd $MYPWD
---
+++ john-the-ripper/john-the-ripper.opencl
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# snap info
+if [ -z ${SNAP+x} ]; then 
+   echo "========================================================"
+   echo "Set the SNAP env var to point to the right location"
+   echo "Something like this: export SNAP=/snap/john-the-ripper/9"
+   echo "========================================================"
+   exit 0
+fi
+
+#export SNAP="/snap/john-the-ripper/9"
+export SNAP_USER_DATA="$HOME$SNAP"
+
+if [ ! -d "$SNAP_USER_DATA" ]; then
+   mkdir -p "$SNAP_USER_DATA"
+fi
+export HOME="$SNAP_USER_DATA"
+
+export PATH="$SNAP/bin:$SNAP/usr/bin:$PATH"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/lib:$SNAP/usr/lib:$SNAP/lib/x86_64-linux-gnu:$SNAP/usr/lib/x86_64-linux-gnu"
+export LD_LIBRARY_PATH="$SNAP/usr/local/lib:$LD_LIBRARY_PATH"
+
+LD_LIBRARY_PATH=$SNAP_LIBRARY_PATH:$LD_LIBRARY_PATH
+exec "$SNAP/run/john-opencl" "$@"
