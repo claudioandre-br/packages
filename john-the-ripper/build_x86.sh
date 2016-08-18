b70a9f619b2f36838acbbaca13ac6961b7a9dcc4
Claudio André <claudioandre.br@gmail.com>
2016-08-18 17:59:17 -0300

    Stop using the deprecated copy plugin.

--- john-the-ripper/build_x86.sh
+++ john-the-ripper/build_x86.sh
@@ -5,8 +5,7 @@
 #rm parts/john-the-ripper/build/run/john-*
 
 git clone https://github.com/magnumripper/JohnTheRipper.git tmp
-mv tmp/* ./
-mv tmp/.git ./
+cp -r tmp/. .
 cd src
 
 wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/0001-Handle-self-confined-system-wide-build.patch
@@ -14,14 +13,14 @@ wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/Temp
 patch < 0001-Handle-self-confined-system-wide-build.patch
 patch < Temporary\ -\ disable\ nice
 
-./configure --disable-native-tests --with-systemwide --disable-openmp CPPFLAGS='-D_SNAP' && make -s clean && make -sj2 && mv ../run/john ../run/john-opencl-non-omp       &&
-./configure --disable-native-tests --with-systemwide CPPFLAGS='-D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-opencl-non-omp\""' && make -s clean && make -sj2 && mv ../run/john ../run/john-opencl   
+./configure --disable-native-tests --with-systemwide --disable-openmp CPPFLAGS='-D_SNAP' && make -s clean && make -s && mv ../run/john ../run/john-opencl-non-omp       &&
+./configure --disable-native-tests --with-systemwide CPPFLAGS='-D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-opencl-non-omp\""' && make -s clean && make -s && mv ../run/john ../run/john-opencl   
 
-./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS='-D_SNAP' && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2-non-omp       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-sse2-non-omp\""' && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx -D_SNAP' --disable-openmp && make -s clean && make -sj2 && mv ../run/john ../run/john-avx-non-omp       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-avx-non-omp\"" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-sse2\""' && make -s clean && make -sj2 && mv ../run/john ../run/john-avx       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mxop -D_SNAP' --disable-openmp && make -s clean && make -sj2 && mv ../run/john ../run/john-xop-non-omp       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mxop -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-xop-non-omp\"" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-avx\""' && make -s clean && make -sj2 && mv ../run/john ../run/john-xop       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx2 -D_SNAP' --disable-openmp && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx2 -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-non-omp\"" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-xop\""' && make -s clean && make -sj2 
+./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS='-D_SNAP' && make -s clean && make -s && mv ../run/john ../run/john-sse2-non-omp       &&
+./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-sse2-non-omp\""' && make -s clean && make -s && mv ../run/john ../run/john-sse2       &&
+./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx -D_SNAP' --disable-openmp && make -s clean && make -s && mv ../run/john ../run/john-avx-non-omp       &&
+./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-avx-non-omp\"" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-sse2\""' && make -s clean && make -s && mv ../run/john ../run/john-avx       &&
+./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mxop -D_SNAP' --disable-openmp && make -s clean && make -s && mv ../run/john ../run/john-xop-non-omp       &&
+./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mxop -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-xop-non-omp\"" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-avx\""' && make -s clean && make -s && mv ../run/john ../run/john-xop       &&
+./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx2 -D_SNAP' --disable-openmp && make -s clean && make -s && mv ../run/john ../run/john-non-omp       &&
+./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-mavx2 -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-non-omp\"" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-xop\""' && make -s clean && make -s 
