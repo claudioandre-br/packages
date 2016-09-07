96bff89d3b1c5476b608d291828b90db6dbc3962
Claudio André <claudioandre.br@gmail.com>
2016-09-07 15:40:09 -0300

    Some tweaks to Travis

--- john-the-ripper/build_arm.sh
+++ john-the-ripper/build_arm.sh
@@ -24,6 +24,7 @@
 
 git clone https://github.com/magnumripper/JohnTheRipper.git tmp
 cp -r tmp/. .
+wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/john-the-ripper.opencl
 cd src
 
 wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/0001-Handle-self-confined-system-wide-build.patch
@@ -31,19 +32,26 @@ wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/Temp
 patch < 0001-Handle-self-confined-system-wide-build.patch
 patch < Temporary\ -\ disable\ nice
 
-if [[ "$TEST" = "yes" ]]; then
-    ./configure --with-systemwide CPPFLAGS='-D_SNAP' && make -s clean && make -s
-    ../run/john --list=build-info
-    ../run/john --stdout --regex='[0-2]password[A-C]'
-    ../run/john -test-full=0 --format=raw-sha256
-    ../run/john -test-full=0 --format=sha512crypt
-
-    exit 0
-fi
-
 # CFLAGS is set when regex is build. Harm JtR configure process.
+TMP_FLAGS="$CFLAGS"
 unset CFLAGS
 
 # CPU (OMP and extensions fallback)
-./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS='-D_SNAP' && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp       &&
-./configure --disable-native-tests --disable-opencl --with-systemwide CPPFLAGS='-D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY="\"john-non-omp\""' && make -s clean && make -sj2 
+./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="$TMP_FLAGS -D_SNAP" && make -s clean && make -s && mv ../run/john ../run/john-non-omp
+./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="$TMP_FLAGS -D_SNAP -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-non-omp\\\"\"" && make -s clean && make -s
+
+# Do some testing
+if [[ "$TEST" = "yes" ]]; then
+    echo ""
+    echo "---------------------------- TESTING -----------------------------"
+    ../run/john --list=build-info
+    echo "------------------------------------------------------------------"
+    ../run/john --stdout --regex='[0-2]password[A-C]'
+    echo "------------------------------------------------------------------"
+    ../run/john -test-full=0 --format=nt
+    echo "------------------------------------------------------------------"
+    ../run/john -test-full=0 --format=raw-sha256
+    echo "------------------------------------------------------------------"
+    echo ""
+fi
+
