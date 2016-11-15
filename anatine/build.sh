2ee054e44510d6ff7cca35e88d6c6e6dc3d87c83
Claudio André <claudioandre.br@gmail.com>
2016-11-15 18:46:43 -0200

    anatine: add a new snap Anatine, a Twitter client

---
+++ anatine/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+git clone https://github.com/sindresorhus/anatine.git tmp
+cp -r tmp/. .
+
+arch=`uname -m`
+text='x'
+git_tag=$(git describe --dirty=+ --always 2>/dev/null)
+
+case "$arch" in
+    'x86_64')
+        text='J1'
+        ;;
+    'armhf' | 'armv7l')
+        text='a1'
+        ;;
+    'aarch64' | 'arm64')
+        text='B1'
+        ;;
+    'ppc64le' | 'powerpc64le')
+        text='P1'
+        ;;
+esac
+# Set package version
+sed -i "s/edge/$git_tag-$text/g" ../../../snapcraft.yaml
+
+npm install
+npm run build:linux
+cp -r dist/Anatine-linux-x64/. ../install/bin/
+
