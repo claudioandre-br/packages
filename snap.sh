600541e9a6d7c994e095e1fe587cc764c10b98b5
Claudio André <claudioandre.br@gmail.com>
2016-11-18 13:24:38 -0200

    anatine: use Travis as a build machine

---
+++ snap.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+if [ "$SNAP" != "yes" ]; then
+    exit 0
+fi
+
+if [ -z "$SNAPCRAFT_SECRET" ]; then
+    exit 0
+fi
+
+echo ""
+echo "---------------------------- PACKAGING -----------------------------"
+
+mkdir -p ".encrypted"
+if [ ! -e ".encrypted/snapcraft.cfg.enc" ]; then
+    echo "Seeding a new macaroon."
+    echo "$SNAPCRAFT_CONFIG" > ".encrypted/snapcraft.cfg.enc"
+fi
+
+mkdir -p "$HOME/.config/snapcraft"
+openssl enc -aes-256-cbc -base64 -pass env:SNAPCRAFT_SECRET -d -in ".encrypted/snapcraft.cfg.enc" -out "$HOME/.config/snapcraft/snapcraft.cfg"
+
+if docker run -v $HOME:/root -v $(pwd):/cwd didrocks/snapcraft sh -c 'cd /cwd; snapcraft'; then
+    if [ "${TRAVIS_BRANCH}" = "daily" ]; then
+        docker run -v $HOME:/root -v $(pwd):/cwd didrocks/snapcraft sh -c "cd /cwd; snapcraft push *.snap --release edge"
+    elif [ "${TRAVIS_BRANCH}" = "publish" ]; then
+        docker run -v $HOME:/root -v $(pwd):/cwd didrocks/snapcraft sh -c "cd /cwd; snapcraft push *.snap"
+    fi
+fi
+
+openssl enc -aes-256-cbc -base64 -pass env:SNAPCRAFT_SECRET -out ".encrypted/snapcraft.cfg.enc" < "$HOME/.config/snapcraft/snapcraft.cfg"
+rm -f "$HOME/.config/snapcraft/snapcraft.cfg"
