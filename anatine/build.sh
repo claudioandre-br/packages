f2b0e656ad5ef0b8f855465d06dd2b29ca635524
Claudio André <claudioandre.br@gmail.com>
2017-01-06 10:00:59 -0200

    anatine: add localization

--- anatine/build.sh
+++ anatine/build.sh
@@ -3,9 +3,14 @@
 git clone https://github.com/sindresorhus/anatine.git tmp
 cp -r tmp/. .
 
+wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/0001-localization-adapt-the-software-to-different-languag.patch
+
 arch=`uname -m`
 text='x'
 git_tag=$(git describe --dirty=+ --always 2>/dev/null)
+
+# Localization
+git am 0001-localization-adapt-the-software-to-different-languag.patch
 
 case "$arch" in
     'x86_64')
@@ -22,8 +27,11 @@ case "$arch" in
         ;;
 esac
 # Set package version
-sed -i "s/edge/$git_tag-$text/g" ../../../snapcraft.yaml
+sed -i "s/edge/$git_tag-$text+/g" ../../../snapcraft.yaml
 
 npm install
 nodejs ./node_modules/.bin/electron-packager . --out=dist --ignore='^media$' --platform=linux
-cp -r dist/Anatine-linux-x64/. ../install/bin/
+cp -r dist/Anatine-linux*/. ../install/bin/
+cp -r static/. ../install/bin/static/
+cp -r locales/*.json ../install/bin/locales/
+
