7a0659474ef0314e725c00016646e11f69b0afd9
Claudio André <claudioandre.br@gmail.com>
2017-01-08 10:24:21 -0200

    anatine: build and package for armhf

--- anatine/build.sh
+++ anatine/build.sh
@@ -15,22 +15,26 @@ git am 0001-localization-adapt-the-software-to-different-languag.patch
 case "$arch" in
     'x86_64')
         text='X'
+        arch_id='x64'
         ;;
     'armhf' | 'armv7l')
         text='a'
+        arch_id='armv7l'
         ;;
     'aarch64' | 'arm64')
         text='B'
+        arch_id='unsupported'
         ;;
     'ppc64le' | 'powerpc64le')
         text='P'
+        arch_id='unsupported'
         ;;
 esac
 # Set package version
 sed -i "s/edge/$git_tag-$text+/g" ../../../snapcraft.yaml
 
 npm install
-nodejs ./node_modules/.bin/electron-packager . --out=dist --ignore='^media$' --platform=linux
+nodejs ./node_modules/.bin/electron-packager . --out=dist --ignore='^media$' --platform=linux --arch="$arch_id"
 cp -r dist/Anatine-linux*/. ../install/bin/
 cp -r static/. ../install/bin/static/
 cp -r locales/*.json ../install/bin/locales/
