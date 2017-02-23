f4d19f8921b2fc561f725a3f8f521a6c92fb90a5
Claudio André <claudioandre.br@gmail.com>
2017-02-23 12:13:14 -0300

    JtR: disable a buggy format

--- john-the-ripper/tests/buggy.sh
+++ john-the-ripper/tests/buggy.sh
@@ -17,6 +17,24 @@ fi
 #bcrypt BF_fmt.c
 FILES="  pbkdf2-hmac-md4_fmt_plug.c pbkdf2-hmac-md5_fmt_plug.c opencl_pbkdf2_hmac_md4_fmt_plug.c opencl_pbkdf2_hmac_md5_fmt_plug.c opencl_bf_fmt_plug.c opencl_DES_fmt_plug.c opencl_DES_bs*  opencl_gpg_fmt_plug.c opencl_krb5pa-md5_fmt_plug.c opencl_mscash2_fmt_plug.c opencl_nt_fmt_plug.c opencl_ntlmv2_fmt_plug.c opencl_o5logon_fmt_plug.c opencl_rawmd5_fmt_plug.c opencl_rawmd4_fmt_plug.c opencl_xsha512_fmt_plug.c opencl_mysqlsha1_fmt_plug.c opencl_mscash_fmt_plug.c"
 
+#BCrypt
+patch -l -f -p2 <<ENDPATCH
+diff --git a/src/john.c b/src/john.c
+index 285ce89..219edbe 100644
+--- a/src/john.c
++++ b/src/john.c
+@@ -362,7 +362,7 @@ static void john_register_all(void)
+ 	john_register_one(&fmt_DES);
+ 	john_register_one(&fmt_BSDI);
+ 	john_register_one(&fmt_MD5);
+-	john_register_one(&fmt_BF);
++	//john_register_one(&fmt_BF);
+ 	john_register_one(&fmt_scrypt);
+ 	john_register_one(&fmt_LM);
+ 	john_register_one(&fmt_AFS);
+ENDPATCH
+# -----
+
 mkdir -p bugged
 Total_Formats=0
 
