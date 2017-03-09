0a99f5389e120ca17a8743888a95cdbb463f9625
Claudio André <claudioandre.br@gmail.com>
2017-03-09 12:13:14 -0300

    JtR: stop to use disabled formats

--- john-the-ripper/tests/tests.sh
+++ john-the-ripper/tests/tests.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-# There is a bug in echo -e in Travis
-echo '[Disabled:Formats]' > john-local.conf
-echo 'Raw-SHA512-free-opencl = Y' >> john-local.conf
-echo 'XSHA512-free-opencl = Y' >> john-local.conf
-echo 'gpg-opencl = Y' >> john-local.conf
-
 # Proper testing. Trusty AMD GPU drivers on Travis are fragile
 if test "$PROBLEM" = "slow" ; then
     ../run/john -test=0 --format=cpu
