2a639ad563af591caeec7d1963da84c65dc6541c
Claudio André <claudioandre.br@gmail.com>
2017-03-13 12:13:14 -0300

    anatine: do a prepare during build phase

--- anatine/build.sh
+++ anatine/build.sh
@@ -3,34 +3,37 @@
 git clone https://github.com/claudioandre/anatine.git tmp
 cp -r tmp/. .
 
-arch=`uname -m`
-text='x'
-git_tag=$(git describe --dirty=+ --always 2>/dev/null)
+if [[ $1 == "PREPARE" ]]; then
+    arch=`uname -m`
+    text='x'
+    git_tag=$(git describe --dirty=+ --always 2>/dev/null)
 
-case "$arch" in
-    'x86_64')
-        text='X'
-        arch_id='x64'
-        ;;
-    'armhf' | 'armv7l')
-        text='a'
-        arch_id='armv7l'
-        ;;
-    'aarch64' | 'arm64')
-        text='B'
-        arch_id='unsupported'
-        ;;
-    'ppc64le' | 'powerpc64le')
-        text='P'
-        arch_id='unsupported'
-        ;;
-esac
-# Set package version
-sed -i "s/edge/$git_tag+$text/g" ../../../snapcraft.yaml
+    case "$arch" in
+        'x86_64')
+            text='X'
+            arch_id='x64'
+            ;;
+        'armhf' | 'armv7l')
+            text='a'
+            arch_id='armv7l'
+            ;;
+        'aarch64' | 'arm64')
+            text='B'
+            arch_id='unsupported'
+            ;;
+        'ppc64le' | 'powerpc64le')
+            text='P'
+            arch_id='unsupported'
+            ;;
+    esac
+    # Set package version
+    sed -i "s/edge/$git_tag+$text/g" ../../../snapcraft.yaml
 
-npm install
-nodejs ./node_modules/.bin/electron-packager . --out=dist --ignore='^media$' --platform=linux --arch="$arch_id"
-cp -r dist/Anatine-linux*/. ../install/bin/
-cp -r static/. ../install/bin/static/
-cp -r locales/*.json ../install/bin/locales/
+else
+    npm install
+    nodejs ./node_modules/.bin/electron-packager . --out=dist --ignore='^media$' --platform=linux --arch="$arch_id"
+    cp -r dist/Anatine-linux*/. ../install/bin/
+    cp -r static/. ../install/bin/static/
+    cp -r locales/*.json ../install/bin/locales/
+fi
 
