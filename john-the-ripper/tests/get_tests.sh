cc51da01f19be3cce04157f04550e611fea7e740
Claudio André <claudioandre.br@gmail.com>
2017-03-22 12:13:14 -0300

    JtR: add a helper script

--- john-the-ripper/tests/get_tests.sh
+++ john-the-ripper/tests/get_tests.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-rm -rf .travis.yml src/buggy.sh .travis/
+rm -rf .travis.yml circle.yml src/buggy.sh .travis/ .circle/
+
+mkdir -p .circle/
+mkdir -p .travis/
+
+wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/buggy.sh      -P src/
 
 wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/.travis.yml
-wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/buggy.sh      -P src/
 wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/tests.sh      -P .travis/
 wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/travis_ci.sh  -P .travis/
+
+wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/circle.yml    .
+wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/circle-ci.sh  -P .circle/
 
 chmod +x src/buggy.sh
 chmod +x .travis/tests.sh
 chmod +x .travis/travis_ci.sh
+chmod +x .circle/circle-ci.sh
 
 git add .travis.yml
+git add circle.yml
 git add .travis/
+git add .circle/
 git add src/buggy.sh
