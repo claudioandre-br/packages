2733ccd9d77a0ab6fa124862b650f439c7a2b64a
Claudio André <claudioandre.br@gmail.com>
2017-03-26 12:13:14 -0300

    JtR: small tweaks

--- john-the-ripper/tests/get_tests.sh
+++ john-the-ripper/tests/get_tests.sh
@@ -9,7 +9,7 @@ wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-rip
 
 wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/.travis.yml
 wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/tests.sh      -P .travis/
-wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/travis_ci.sh  -P .travis/
+wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/travis-ci.sh  -P .travis/
 
 wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/circle.yml    .
 wget https://raw.githubusercontent.com/claudioandre/packages/master/john-the-ripper/tests/circle-ci.sh  -P .circle/
