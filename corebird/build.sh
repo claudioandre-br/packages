7de81d9ec72d5e086b1f985de21d0af5cf8f9bbf
Claudio André <claudioandre.br@gmail.com>
2017-05-08 12:13:14 -0300

    corebird: fixes for Launchpad (still waiting for a bug in LP)

--- corebird/build.sh
+++ corebird/build.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
-git clone https://github.com/baedert/corebird.git tmp
-cp -r tmp/. .
+if [[ $1 == "PREPARE" ]]; then
+    arch=`uname -m`
+    text='x'
+    git_tag=$(git describe --dirty=+ --always 2>/dev/null)
 
-wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/corebird.patch
-patch -p1 < corebird.patch
+    case "$arch" in
+        'x86_64')
+            text='X'
+            ;;
+        'armhf' | 'armv7l')
+            text='a'
+            ;;
+        'aarch64' | 'arm64')
+            text='B'
+            ;;
+        'ppc64le' | 'powerpc64le')
+            text='P'
+            ;;
+    esac
+    # Set package version
+    sed -i "s/edge/$git_tag+$text/g" ../../../snapcraft.yaml
 
-arch=`uname -m`
-text='x'
-git_tag=$(git describe --dirty=+ --always 2>/dev/null)
+else
+    git clone https://github.com/baedert/corebird.git tmp
+    cp -r tmp/. .
 
-case "$arch" in
-    'x86_64')
-        text='X'
-        ;;
-    'armhf' | 'armv7l')
-        text='a'
-        ;;
-    'aarch64' | 'arm64')
-        text='B'
-        ;;
-    'ppc64le' | 'powerpc64le')
-        text='P'
-        ;;
-esac
-# Set package version
-sed -i "s/edge/$git_tag-$text/g" ../../../snapcraft.yaml
+    wget https://raw.githubusercontent.com/claudioandre/packages/master/patches/corebird.patch
+    patch -p1 < corebird.patch
 
-# Install vala - requer 
-#sudo add-apt-repository ppa:vala-team/ppa -y
-#sudo apt-get update
-#sudo apt-get install vala -y
+    # Install vala - requer 
+    #sudo add-apt-repository ppa:vala-team/ppa -y
+    #sudo apt-get update
+    #sudo apt-get install vala -y
 
-dest=$(dirname "$PWD")
+    dest=$(dirname "$PWD")
 
-./autogen.sh --prefix= --disable-spellcheck
-make
-make install DESTDIR="$dest/install"
+    ./autogen.sh --prefix= --disable-spellcheck
+    make
+    make install DESTDIR="$dest/install"
+fi
