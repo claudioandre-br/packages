d273e66a55f0472c16637cbbc8390d90964a424c
Claudio André <claudioandre.br@gmail.com>
2017-05-21 12:13:14 -0300

    JtR: testing tweaks

--- john-the-ripper/tests/tests.sh
+++ john-the-ripper/tests/tests.sh
@@ -28,7 +28,7 @@ do_Test () {
         if [[ -z $RESULT ]]; then
             echo --------------------------------------------
             echo "Test: ($1) FAILED"
-            tail -n7 $TEMP
+            tail -n10 $TEMP
             echo --------------------------------------------
             echo
             exit 1
@@ -130,7 +130,7 @@ if test "$EXTRAS" = "yes" ; then
     do_Test "$JtR ~/file6 --show --format=raw-md5"                                ""      64  36
     do_Test "$JtR ~/hash --show:left --format=rar"                                ""       1   1
     do_Test "$JtR ~/self -form=SHA512crypt"                             "1g 0:00:00"      -1  -1
-    do_Test "$JtR ~/self -form=raw-SHA512 --incremental -fork=2"        "2g 0:00:00"      -1  -1
+    do_Test "$JtR ~/self -form=raw-SHA512 --incremental -fork=2"         "2g 0:00:0"      -1  -1
     do_Test "$JtR ~/self --show --format=raw-SHA512"                              ""       3   0
 
     do_Test "$JtR ~/self --form=oldoffice --mask=5?d5?a73?A3"                   "1g 0:00:00"      -1  -1
