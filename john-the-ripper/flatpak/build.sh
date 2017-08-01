fffdb49e7088d669c0365ecf13b9aa7ae749bd9f
Claudio André <claudioandre.br@gmail.com>
2017-08-01 12:13:14 -0300

    JtR: re-enable fallback binaries in flatpak package

--- john-the-ripper/flatpak/build.sh
+++ john-the-ripper/flatpak/build.sh
@@ -26,15 +26,14 @@ if [[ "$TEST" != "yes" ]]; then
 
     if [[ "$arch" == "x86_64" ]]; then
         # CPU (OMP and extensions fallback)
-./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED" && make -s clean && make -sj2
-        #./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED" && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2-non-omp
-        #./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-sse2-non-omp\\\"\"" && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2
-        #./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED -mavx" && make -s clean && make -sj2 && mv ../run/john ../run/john-avx-non-omp
-        #./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -mavx -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-avx-non-omp\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-sse2\\\"\"" && make -s clean && make -sj2 && mv ../run/john ../run/john-avx
-        #./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED -mxop" && make -s clean && make -sj2 && mv ../run/john ../run/john-xop-non-omp
-        #./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -mxop -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-xop-non-omp\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-avx\\\"\"" && make -s clean && make -sj2 && mv ../run/john ../run/john-xop
-        #./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED -mavx2" && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp
-        #./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -mavx2 -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-non-omp\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-xop\\\"\"" && make -s clean && make -sj2
+        ./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED" && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2-non-omp
+        ./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-sse2-non-omp\\\"\"" && make -s clean && make -sj2 && mv ../run/john ../run/john-sse2
+        ./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED -mavx" && make -s clean && make -sj2 && mv ../run/john ../run/john-avx-non-omp
+        ./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -mavx -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-avx-non-omp\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-sse2\\\"\"" && make -s clean && make -sj2 && mv ../run/john ../run/john-avx
+        ./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED -mxop" && make -s clean && make -sj2 && mv ../run/john ../run/john-xop-non-omp
+        ./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -mxop -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-xop-non-omp\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-avx\\\"\"" && make -s clean && make -sj2 && mv ../run/john ../run/john-xop
+        ./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED -mavx2" && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp
+        ./configure --disable-native-tests --disable-opencl --with-systemwide                  CPPFLAGS="-D_BOXED -mavx2 -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-non-omp\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-xop\\\"\"" && make -s clean && make -sj2
     else
         # CPU (OMP and extensions fallback)
         ./configure --disable-native-tests --disable-opencl --with-systemwide --disable-openmp CPPFLAGS="-D_BOXED" && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp
