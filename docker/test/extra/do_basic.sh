c7335189341f5ab45ba9e8c851e23b4f5215b1e1
Claudio André <claudioandre.br@gmail.com>
2018-02-26 09:32:22 -0300

    gjs: add a dependency to get some statistics

--- docker/test/extra/do_basic.sh
+++ docker/test/extra/do_basic.sh
@@ -45,7 +45,7 @@ function do_Install_Dependencies(){
         dnf -y -q install gtk3 gtk3-devel gobject-introspection Xvfb gnome-desktop-testing dbus-x11 dbus \
                           cairo intltool libxslt bison nspr zlib python3-devel dbus-glib libicu libffi pcre libxml2 libxslt libtool flex \
                           cairo-devel zlib-devel libffi-devel pcre-devel libxml2-devel libxslt-devel \
-                          libedit-devel libasan libubsan lcov mesa-libGL-devel
+                          libedit-devel libasan libubsan lcov mesa-libGL-devel tokei
     fi
 }
 
