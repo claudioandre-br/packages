482543903cd7bac52b4f34756f43edfb80d87c63
Claudio André <claudioandre.br@gmail.com>
2018-07-24 12:40:23 -0300

    JtR: add a test of drupal7-ztex

--- john-the-ripper/tests/tests.sh
+++ john-the-ripper/tests/tests.sh
@@ -265,6 +265,7 @@ else
         do_Test "$JtR -test=0 --format=descrypt-ztex" "no valid ZTEX devices found" 0 0 "ERROR"
         do_Test "$JtR -test=0 --format=bcrypt-ztex"   "no valid ZTEX devices found" 0 0 "ERROR"
         do_Test "$JtR -test=0 --format=sha512crypt-ztex"   "no valid ZTEX devices found" 0 0 "ERROR"
+        do_Test "$JtR -test=0 --format=drupal7-ztex"   "no valid ZTEX devices found" 0 0 "ERROR"
     elif test "$PROBLEM" = "slow" -a "$CC" = "gcc" ; then
         echo "$ JtR -test=0 --format=cpu"
         "$JtR" -test=0 --format=cpu
