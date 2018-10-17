077e0e1b162cbf8b9d9c2ec87686cbefe7ee673c
Claudio André <claudioandre.br@gmail.com>
2018-10-17 12:29:47 -0300

    JtR: allow to compare SIMD and non-SIMD builds
    Some tests seem to demonstrate that SIMD is worse than non-SIMD builds
    (inside Launchpad).

--- john-the-ripper/snap/build.sh
+++ john-the-ripper/snap/build.sh
@@ -77,6 +77,45 @@ else
     ./configure --with-systemwide --disable-openmp CPPFLAGS="-D_SNAP -D_BOXED" && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp
     ./configure --with-systemwide                  CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-non-omp\\\"\"" && make -s clean && make -sj2
 
+    if false; then
+        # Rename binaries
+        mv ../run/john-non-omp ../run/john-neon-no-omp
+        mv ../run/john ../run/john-neon-omp
+
+        # Allow to compare SIMD and non-SIMD builds
+        ./configure --disable-native-tests --disable-simd --with-systemwide --disable-openmp CPPFLAGS="-D_SNAP -D_BOXED" && make -s clean && make -sj2 && mv ../run/john ../run/john-non-omp
+        ./configure --disable-native-tests --disable-simd --with-systemwide                  CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-non-omp\\\"\"" && make -s clean && make -sj2
+
+        # Rename new binaries
+        mv ../run/john-non-omp ../run/john-no-omp
+        mv ../run/john ../run/john-omp
+
+        # Test sha256crypt
+        ../run/john-no-omp -test -form:sha256crypt -tune=1
+        ../run/john-neon-no-omp -test -form:sha256crypt -tune=1
+
+        ../run/john-omp -test -form:sha256crypt -tune=1
+        ../run/john-neon-omp -test -form:sha256crypt -tune=1
+
+        # Test sha512crypt
+        ../run/john-no-omp -test -form:sha512crypt -tune=1
+        ../run/john-neon-no-omp -test -form:sha512crypt -tune=1
+
+        ../run/john-omp -test -form:sha512crypt -tune=1
+        ../run/john-neon-omp -test -form:sha512crypt -tune=1
+
+        # Test something else
+        ../run/john-no-omp -test -tune=1 -form:pbkdf2-hmac-md5
+        ../run/john-neon-no-omp -test -tune=1 -form:pbkdf2-hmac-md5
+
+        ../run/john-omp -test -tune=1 -form:pbkdf2-hmac-md5
+        ../run/john-neon-omp -test -tune=1 -form:pbkdf2-hmac-md5
+
+        # Nothing else to do
+        ln -s ../run/john-neon-omp ../run/john
+        ln -s ../run/john ../run/john-opencl
+        exit 0
+    fi
     # Workaround for non X86
     ln -s ../run/john ../run/john-opencl
 fi
