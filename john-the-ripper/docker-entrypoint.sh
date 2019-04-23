b3e849d7a21c91775b65780989c762022a7cb911
Claudio André <claudioandre.br@gmail.com>
2019-04-23 09:34:23 -0300

    JtR: add SIMD binaries to Docker image
    For comparison purposes. To pick up a SIMD, run:
    - docker run -it claudioandre/john:v1.9.0J1 sse2 #The default binary
    - docker run -it claudioandre/john:v1.9.0J1 ssse3-no-omp
    - docker run -it claudioandre/john:v1.9.0J1 avx2

---
+++ john-the-ripper/docker-entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+######################################################################
+# Copyright (c) 2019 Claudio André <claudioandre.br at gmail.com>
+#
+# This program comes with ABSOLUTELY NO WARRANTY; express or implied.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, as expressed in version 2, seen at
+# http://www.gnu.org/licenses/gpl-2.0.html
+######################################################################
+
+set -e
+echo "$@"
+binaries="sse2 sse2-no-omp ssse3 ssse3-no-omp sse4.1 sse4.1-no-omp sse4.2 sse4.2-no-omp
+          avx avx-no-omp xop avx-no-omp avx2 avx2-no-omp
+          avx512f avx512f-no-omp avx512bw avx512bw-no-omp
+          "
+binary="$1"
+
+if [[ $# -gt 0 && "$binaries" == *"$binary"* ]]; then
+    shift
+fi
+
+if [[ "$binary" = 'sse2-no-omp' ]]; then
+    exec /john/run/john-sse2-no-omp "$@"
+elif [[ "$binary" = 'ssse3' ]]; then
+    exec /john/run/john-ssse3 "$@"
+elif [[ "$binary" = 'ssse3-no-omp' ]]; then
+    exec /john/run/john-ssse3-no-omp "$@"
+elif [[ "$binary" = 'sse4.1' ]]; then
+    exec /john/run/john-sse4.1 "$@"
+elif [[ "$binary" = 'sse4.1-no-omp' ]]; then
+    exec /john/run/john-sse4.1-no-omp "$@"
+elif [[ "$binary" = 'sse4.2' ]]; then
+    exec /john/run/john-sse4.2 "$@"
+elif [[ "$binary" = 'sse4.2-no-omp' ]]; then
+    exec /john/run/john-sse4.2-no-omp "$@"
+elif [[ "$binary" = 'avx' ]]; then
+    exec /john/run/john-avx "$@"
+elif [[ "$binary" = 'avx-no-omp' ]]; then
+    exec /john/run/john-avx-no-omp "$@"
+elif [[ "$binary" = 'xop' ]]; then
+    exec /john/run/john-xop "$@"
+elif [[ "$binary" = 'xop-no-omp' ]]; then
+    exec /john/run/john-xop-no-omp "$@"
+elif [[ "$binary" = 'avx2' ]]; then
+    exec /john/run/john-avx2 "$@"
+elif [[ "$binary" = 'avx2-no-omp' ]]; then
+    exec /john/run/john-avx2-no-omp "$@"
+elif [[ "$binary" = 'avx512f' ]]; then
+    exec /john/run/john-avx512f "$@"
+elif [[ "$binary" = 'avx512f-no-omp' ]]; then
+    exec /john/run/john-avx512f-no-omp "$@"
+elif [[ "$binary" = 'avx512bw' ]]; then
+    exec /john/run/john-avx512bw "$@"
+elif [[ "$binary" = 'avx512bw-no-omp' ]]; then
+    exec /john/run/john-avx512bw-no-omp "$@"
+else
+    exec /john/run/john-sse2 "$@"
+fi
+message='### See you soon! ###'
+printf "%*s\n" $(((${#message} + $(tput cols)) / 2)) "$message"
+
--- john-the-ripper/readme.md
+++ john-the-ripper/readme.md
@@ -325,30 +325,50 @@ SHA256          228055F2CF88F055A657BCAE3E3A898298B3AEC5766534D2B567D8993F71D2D5
 
 > Built using Travis CI and deployed using Docker Hub
 
-For future reference, we have a Docker image of John the Ripper Jumbo 1.9.0.J1
-available.
-
-In order to use and test it:
+For testing and future reference, we have a Docker image of John the Ripper
+Jumbo 1.9.0.J1. In order to use and test it:
 
 ```bash
  # CPU only formats
- docker run -it claudioandre/john:v1.9.0J1 <options>
+ docker run -it claudioandre/john:v1.9.0J1 <binary id> <john options>
 
  # To run ztex formats
- docker run -it --device=/dev/ttyUSB0 claudioandre/john:v1.9.0J1 <options>
+ docker run -it --device=/dev/ttyUSB0 claudioandre/john:v1.9.0J1 <binary id> <john options>
 ```
 
 Run John the Ripper and check if it is working:
 
 ```bash
- docker run -it claudioandre/john:v1.9.0J1
- docker run -it claudioandre/john:v1.9.0J1 -list=build-info
- docker run -it claudioandre/john:v1.9.0J1 -format=cpu
+ docker run -it claudioandre/john:v1.9.0J1 # => SSE2
  docker run -it claudioandre/john:v1.9.0J1 -list=format-tests | cut -f3 > ~/alltests.in
- docker run -it -v "$HOME":/host claudioandre/john:v1.9.0J1 -form=SHA512crypt /host/alltests.in --max-run=300
+ docker run -it claudioandre/john:v1.9.0J1 ssse3-no-omp -list=build-info
+ docker run -it claudioandre/john:v1.9.0J1 avx2 -test=0 -format=cpu
+ docker run -it claudioandre/john:v1.9.0J1 avx512bw
+ docker run -it -v "$HOME":/host claudioandre/john:v1.9.0J1 avx -form=SHA512crypt /host/alltests.in --max-run=300
 ```
 
 The highlights:
 
 - ztex formats available;
 - prince mode available.
+
+These binaries are available. Their ids are sse2, sse2-no-omp, ssse3, etc:
+
+- /john/run/john-sse2 (the default binary)
+- /john/run/john-sse2-no-omp
+- /john/run/john-ssse3
+- /john/run/john-ssse3-no-omp
+- /john/run/john-sse4.1
+- /john/run/john-sse4.1-no-omp
+- /john/run/john-sse4.2
+- /john/run/john-sse4.2-no-omp
+- /john/run/john-avx
+- /john/run/john-avx-no-omp
+- /john/run/john-xop
+- /john/run/john-xop-no-omp
+- /john/run/john-avx2
+- /john/run/john-avx2-no-omp
+- /john/run/john-avx512f
+- /john/run/john-avx512f-no-omp
+- /john/run/john-avx512bw
+- /john/run/john-avx512bw-no-omp
